715e0f29ccdf09e80248f4b58bc413dff35e0cdd
Aleki <root@ZJCLOE>
2024-03-08 14:17:58 +0300

    Block Scope

---
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -0,0 +1,10 @@
+export default function taskBlock(trueOrFalse) {
+	let task = false;
+	let task2 = true;
+
+	if (trueOrFalse) {
+		let taskInside = true;
+		let task2Inside = false;
+	}
+	return [task, task2];
+}
