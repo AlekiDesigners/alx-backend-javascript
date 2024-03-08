03518c9162ad215b17610fb230cbf602c3d53eb3
Alex Designer <karuri1.k1@gmail.com>
2024-03-08 15:59:13 +0300

    Keep every promise you make and only make promises you can keep

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,12 @@
+ function getResponseFromAPI() {
+     return new Promise((resolve, reject) => {
+             // Simulating an asynchronous operation (e.g., fetching data from an API)
+                     setTimeout(() => {
+                                 // Resolve the Promise after 1 second
+                                             resolve('Response from API');
+                                                     }, 1000);
+                                                         });
+                                                         }
+
+                                                         export default getResponseFromAPI;
+
